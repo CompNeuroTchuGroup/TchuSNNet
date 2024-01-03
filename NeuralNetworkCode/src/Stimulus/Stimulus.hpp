74e4fd8b597f5837cd70c70d10b6aee78fa0a77d
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-03 17:11:28 +0100

    Stimulus formatting

--- NeuralNetworkCode/src/Stimulus/Stimulus.hpp
+++ NeuralNetworkCode/src/Stimulus/Stimulus.hpp
@@ -1,22 +1,21 @@
-#ifndef Stimulus_HPP
-#define Stimulus_HPP
+#ifndef _STIMULUS_BASE_CLASS_HPP
+#define _STIMULUS_BASE_CLASS_HPP
 
 #include "../GlobalFunctions.hpp"
 #include "../NeuronPopSample.hpp"
 #include <algorithm>
-#include <string>
 #include <iostream>
-#include <vector>
 #include <random>
-#include <fstream>
 #include <ranges>
+#include <string>
+#include <vector>
 
-//Struct used in some of the Stimulus classes to coordinate the input current
+// Struct used in some of the Stimulus classes to coordinate the input current
 struct StepStruct {
-    TStepInt    startTimeStep{};
-    TStepInt    endTimeStep {LONG_MAX};
-    std::vector<double> parameterValues;
-} ;
+  TStepInt            startTimeStep{};
+  TStepInt            endTimeStep{LONG_MAX};
+  std::vector<double> parameterValues;
+};
 /* class Stimulus is a virtual base class for injecting a determined
  * current into each neuron during each time step.
  * - double current(int neuronId, int populationId) returns the current for the
@@ -27,32 +26,30 @@ struct StepStruct {
  *   normalized version of the input current as specified.
  */
 class Stimulus {
-    
+
 protected:
+  GlobalSimInfo                   *infoGlobal;
+  std::shared_ptr<NeuronPopSample> neurons;
+  std::vector<std::vector<double>> signalMatrix;
+  int                              seed;
+  std::mt19937                     generator;
+  bool                             userSeed{false};
 
-    GlobalSimInfo*   infoGlobal;
-    std::shared_ptr<NeuronPopSample> neurons;
-    std::vector<std::vector<double>> signalMatrix;
-    int seed;
-    std::mt19937 generator;
-    bool userSeed {false};
-
-    bool wellDefined{true};
-    virtual void SetSignalMatrix() = 0;
-    virtual void PostLoadParameters() = 0;
+  bool         wellDefined{true};
+  virtual void SetSignalMatrix()    = 0;
+  virtual void PostLoadParameters() = 0;
 
 public:
+  Stimulus(std::shared_ptr<NeuronPopSample> neurons, GlobalSimInfo *infoGlobal);
+  virtual ~Stimulus() = default;
 
-    Stimulus(std::shared_ptr<NeuronPopSample>  neurons,GlobalSimInfo*  infoGlobal);
-    virtual ~Stimulus() = default;
+  virtual void        Update(std::vector<std::vector<double>> &synaptic_dV);
+  virtual std::string GetType() const                    = 0;
+  virtual double      GetScaling(PopInt neuronPop) const = 0;
 
-    virtual void        Update(std::vector<std::vector<double>>& synaptic_dV) ;
-    virtual std::string GetType() const = 0;
-   	virtual double GetScaling(PopInt neuronPop) const = 0;
+  virtual void SaveParameters(std::ofstream &wParameterStream) const;
+  virtual void LoadParameters(const std::vector<FileEntry> &input);
 
-    virtual void SaveParameters(std::ofstream& wParameterStream) const;
-    virtual void LoadParameters(const std::vector<FileEntry>& input);
-
-    double GetSignalMatrixPoint(PopInt neuronPop,NeuronInt neuron) const {return signalMatrix.at(neuronPop).at(neuron);}
+  double GetSignalMatrixPoint(PopInt neuronPop, NeuronInt neuron) const { return signalMatrix.at(neuronPop).at(neuron); }
 };
-#endif //Stimulus_HPP
+#endif // Stimulus_HPP
