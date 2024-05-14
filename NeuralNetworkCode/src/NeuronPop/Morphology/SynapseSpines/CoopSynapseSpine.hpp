a3d87e2229c1b0b9f515d2c64ec859efb19533ab
Antoni Bertolin <tonibm1220@gmail.com>
2024-05-14 18:28:33 +0200

    Update CoopSynapseSpine.hpp

--- NeuralNetworkCode/src/NeuronPop/Morphology/SynapseSpines/CoopSynapseSpine.hpp
+++ NeuralNetworkCode/src/NeuronPop/Morphology/SynapseSpines/CoopSynapseSpine.hpp
@@ -4,35 +4,34 @@
 #ifndef _SYNAPSE_SPINE_COOP_CLASS_HPP
 #define _SYNAPSE_SPINE_COOP_CLASS_HPP
 
-#include "BaseSynapseSpine.hpp"
 #include <string>
 #include <vector>
+#include "BaseSynapseSpine.hpp"
 
-class CoopSynapseSpine : public BaseSynapseSpine {
+struct CoopSynapseSpine : public BaseSynapseSpine {  // TODO: turn into struct, remove getters and such
+  protected:
+    double distToSoma {};
+    double theta {};  // heterosynaptic cooperativity
+    double lastSpike {};
 
-protected:
-  double distToSoma{};
-  double theta{}; // heterosynaptic cooperativity
-  double lastSpike{};
+  public:
+    CoopSynapseSpine() = default;
+    // CoopSynapseSpine(double distToSoma, double lastSpike, double weight);
 
-public:
-  CoopSynapseSpine() = default;
-  // CoopSynapseSpine(double distToSoma, double lastSpike, double weight);
+    // getters
+    double GetDistToSoma() const { return distToSoma; };
+    double GetTheta() const { return theta; };
+    double GetLastSpike() const { return lastSpike; };
+    // setters
+    void SetDistToSoma(double dist) { distToSoma = dist; };
+    void SetTheta(double thetaIn) { theta = thetaIn; };
+    void SetLastSpike(double time) { lastSpike = time; };
+    // Misc
+    void AddToTheta(double hEffect) { theta += hEffect; }
 
-  // getters
-  double GetDistToSoma() const { return distToSoma; };
-  double GetTheta() const { return theta; };
-  double GetLastSpike() const { return lastSpike; };
-  // setters
-  void SetDistToSoma(double dist) { distToSoma = dist; };
-  void SetTheta(double thetaIn) { theta = thetaIn; };
-  void SetLastSpike(double time) { lastSpike = time; };
-  // Misc
-  void AddToTheta(double hEffect) { theta += hEffect; }
-
-  // Profile function
-  std::vector<double> GetIndividualSynapticProfile() const override;
-  std::string         GetIndividualSynapticProfileHeaderInfo() const override;
+    // Profile function
+    std::vector<double> GetIndividualSynapticProfile() const override;
+    std::string         GetIndividualSynapticProfileHeaderInfo() const override;
 };
 
 #endif
