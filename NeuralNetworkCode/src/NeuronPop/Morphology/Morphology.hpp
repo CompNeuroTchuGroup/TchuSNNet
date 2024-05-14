503f0d0484dee43793b4170c906f38b9bf92ee05
Antoni Bertolin <tonibm1220@gmail.com>
2024-05-14 18:13:39 +0200

    Formatting and removed Gapdelay

--- NeuralNetworkCode/src/NeuronPop/Morphology/Morphology.hpp
+++ NeuralNetworkCode/src/NeuronPop/Morphology/Morphology.hpp
@@ -4,78 +4,73 @@
 #ifndef _MORPHOLOGY_BASE_CLASS_HPP
 #define _MORPHOLOGY_BASE_CLASS_HPP
 
-#include "../Morphology/SynapseSpines/BaseSynapseSpine.hpp"
-#include "./../../GlobalFunctions.hpp"
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
+#include "../Morphology/SynapseSpines/BaseSynapseSpine.hpp"
+#include "./../../GlobalFunctions.hpp"
 
-enum WeightNormalization {
-  NOPNormalization,
-  HardNormalization,
-  SoftMaxNormalization
-};
+enum WeightNormalization { NOPNormalization, HardNormalization, SoftMaxNormalization };
 
 class Morphology {
+  protected:
+    GlobalSimInfo *infoGlobal;
 
-protected:
-  GlobalSimInfo *infoGlobal;
+    std::vector<BaseSpinePtr> baseSpineData;
 
-  std::vector<BaseSpinePtr> baseSpineData;
+    // double weightsSum {};
+    double totalPostSpikes {};
+    double totalPreSpikes {};
+    bool   postSpiked { false };
 
-  // double weightsSum {};
-  double totalPostSpikes{};
-  double totalPreSpikes{};
-  bool   postSpiked{false};
+    bool         distributeWeights { false };
+    bool         userSeed { false };
+    int          seed { 0 };
+    std::mt19937 generator;
 
-  bool         distributeWeights{false};
-  bool         userSeed{false};
-  int          seed{0};
-  std::mt19937 generator;
+    double minWeight { 0.0 };
+    double initialWeights { 1.0 };
+    double maxWeight { 2.0 };
 
-  double minWeight{0.0};
-  double initialWeights{1.0};
-  double maxWeight{2.0};
+    virtual void Reset() = 0;
 
-  virtual void Reset() = 0;
+    std::mutex _presynSpikeMutex;
 
-  std::mutex _presynSpikeMutex;
+  public:
+    explicit Morphology(GlobalSimInfo *infoGlobal, const std::vector<FileEntry> &morphologyParameters);
+    virtual ~Morphology() = default;
 
-public:
-  explicit Morphology(GlobalSimInfo *infoGlobal, const std::vector<FileEntry> &morphologyParameters);
-  virtual ~Morphology() = default;
+    virtual void SaveParameters(std::ofstream &wParameterStream, std::string neuronIdentificator) const;
+    virtual void LoadParameters(const std::vector<FileEntry> &parameters);
+    virtual void CheckParameters(const std::vector<FileEntry> &parameters);
 
-  virtual void SaveParameters(std::ofstream &wParameterStream, std::string neuronIdentificator) const;
-  virtual void LoadParameters(const std::vector<FileEntry> &parameters);
-  virtual void CheckParameters(const std::vector<FileEntry> &parameters);
+    virtual BaseSpinePtr AllocateNewSynapse(BranchTargeting &synapse) = 0;
+    double               GenerateSynapticWeight();  // Here we generate the synaptic weight to be allocated when a synapse is allocated
+    SynInt               GetNoSynapses() const { return static_cast<SynInt>(baseSpineData.size()); }
+    virtual std::string  GetType() const = 0;
 
-  virtual BaseSpinePtr AllocateNewSynapse(BranchTargeting &synapse) = 0;
-  double               GenerateSynapticWeight(); // Here we generate the synaptic weight to be allocated when a synapse is allocated
-  SynInt               GetNoSynapses() const { return static_cast<SynInt>(baseSpineData.size()); }
-  virtual std::string  GetType() const = 0;
+    virtual void Advect() = 0;
+    virtual void RecordPostSpike();
+    virtual void RecordExcitatoryPreSpike(BaseSpinePtr spinePtr) = 0;
+    // Record functions
+    std::vector<double> GetIndividualSynapticProfile(signed long synapseId) const;
+    std::string         GetIndividualSynapticProfileHeaderInfo() const;
 
-  virtual void Advect() = 0;
-  virtual void RecordPostSpike();
-  virtual void RecordExcitatoryPreSpike(BaseSpinePtr spinePtr) = 0;
-  // Record functions
-  std::vector<double> GetIndividualSynapticProfile(signed long synapseId) const;
-  std::string         GetIndividualSynapticProfileHeaderInfo() const;
+    virtual std::vector<double> GetOverallSynapticProfile() const;
+    virtual std::string         GetOverallSynapticProfileHeaderInfo() const;
 
-  virtual std::vector<double> GetOverallSynapticProfile() const;
-  virtual std::string         GetOverallSynapticProfileHeaderInfo() const;
+    virtual std::vector<std::pair<std::string, double>> GetSteadyStateData() const;
+    // virtual void CalcMorphoPlasticityEvents() {return;};
+    // friend std::vector<signed long> getSpikedSynapsesFromMorphology(const Morphology&); // This function is not
+    // necessary as the spikedSynapses is not used outside of the class
+    //  signed long GetSynapseCount() const;
+    // virtual int    GetMaxGapDelay(int delayPerMicroMeter) { return 0; }
+    // virtual double GetSynapticDistanceToSoma(int synapseId) = 0;
+    virtual void PostConnectSetUp() {};
 
-  virtual std::vector<std::pair<std::string, double>> GetSteadyStateData() const;
-  // virtual void CalcMorphoPlasticityEvents() {return;};
-  // friend std::vector<signed long> getSpikedSynapsesFromMorphology(const Morphology&); // This function is not
-  // necessary as the spikedSynapses is not used outside of the class
-  //  signed long GetSynapseCount() const;
-  virtual int    GetMaxGapDelay(int delayPerMicroMeter) { return 0; }
-  virtual double GetSynapticDistanceToSoma(int synapseId) = 0;
-  virtual void   PostConnectSetUp(){};
-
-  virtual bool IgnoreJDParameters() const { return false; }
-  virtual bool HasSteadyState() const { return false; }
+    virtual bool IgnoreJcoupling() const { return false; }
+    virtual bool HasSteadyState() const { return false; }
 };
 
-#endif // NEURALNETWORK_MORPHOLOGY_H
+#endif  // NEURALNETWORK_MORPHOLOGY_H
--- NeuralNetworkCode/src/NeuronPop/Morphology/TimingDependentModels/MonoDendriteSTDP.hpp
+++ NeuralNetworkCode/src/NeuronPop/Morphology/TimingDependentModels/MonoDendriteSTDP.hpp
@@ -7,102 +7,102 @@
 #ifndef _MONODENDRITE_BASE_CLASS_HPP
 #define _MONODENDRITE_BASE_CLASS_HPP
 
-#include "../Morphology.hpp"
-#include "../SynapseSpines/CoopSynapseSpine.hpp"
 #include <algorithm>
 #include <iostream>
 #include <numeric>
 #include <string>
+#include "../Morphology.hpp"
+#include "../SynapseSpines/CoopSynapseSpine.hpp"
 
 class MonoDendriteSTDP : public Morphology {
-protected:
-  std::vector<signed long> spikedSpinesId;
-  std::vector<bool>        spikedSpines;
-  // std::vector<std::pair<signed long, double>> preSpikes;
-  // std::vector<double> postSpikes;
-  // std::vector<std::pair<signed long, double>> thetaChanges;
-  // std::vector<std::pair<signed long, double>> weightChanges;
-  // End
-  double tauTheta{};   // decay constant of heterosynaptic effects in spines
-  double lambdaDist{}; // decay constant of heterosynaptic effects over distance between synapses
-  double tauDelay{};   // decay constant of heterosynaptic effects over inter-synapse spike timing difference
+  protected:
+    std::vector<signed long> spikedSpinesId;
+    std::vector<bool>        spikedSpines;
+    // std::vector<std::pair<signed long, double>> preSpikes;
+    // std::vector<double> postSpikes;
+    // std::vector<std::pair<signed long, double>> thetaChanges;
+    // std::vector<std::pair<signed long, double>> weightChanges;
+    // End
+    double tauTheta {};    // decay constant of heterosynaptic effects in spines
+    double lambdaDist {};  // decay constant of heterosynaptic effects over distance between synapses
+    double tauDelay {};    // decay constant of heterosynaptic effects over inter-synapse spike timing difference
 
-  double lastPostSpikeTime{-200};
+    double lastPostSpikeTime { -200 };
 
-  double thetaExpDecay{1.0};
-  double dendriticLength{}; // this would change in case of more complex dendritic geometry (atm it is a single 1D dendrite)
-  double synapticGap{};     //  minimum gap between syanpses along dendrite
+    double thetaExpDecay { 1.0 };
+    double dendriticLength {};  // this would change in case of more complex dendritic geometry (atm it is a single 1D dendrite)
+    double synapticGap {};      //  minimum gap between syanpses along dendrite
 
-  double preFactorLTP{};
-  double preFactorLTD{};
+    double preFactorLTP {};
+    double preFactorLTD {};
 
-  signed long              spineIdGenerator{}; // variable used to allocate new synapses
-  double                   nextPos{};
-  bool                     stepWeights{};
-  std::vector<signed long> weightStepBoundary{};
-  std::vector<double>      weightStepValue{};
-  signed long              currWightStepId{};
+    signed long              spineIdGenerator {};  // variable used to allocate new synapses
+    double                   nextPos {};
+    bool                     stepWeights {};
+    std::vector<signed long> weightStepBoundary {};
+    std::vector<double>      weightStepValue {};
+    signed long              currWightStepId {};
 
-  std::vector<bool> integratePostSpike{};
-  std::vector<bool> integratePreSpike{};
+    std::vector<bool> integratePostSpike {};
+    std::vector<bool> integratePreSpike {};
 
-  std::vector<CoopSpinePtr> spineDataCoop;
+    std::vector<CoopSpinePtr> spineDataCoop;
 
-  double initialWeights{1.0};
+    double initialWeights { 1.0 };
 
-  bool   decayWeights{false};
-  double WeightDecayConstant{1.0};
-  double weightExpDecay{};
+    bool   decayWeights { false };
+    double WeightDecayConstant { 1.0 };
+    double weightExpDecay {};
 
-  WeightNormalization weightNormalization{NOPNormalization};
-  double              softMaxMultiplier{2.0};
+    WeightNormalization weightNormalization { NOPNormalization };
+    double              softMaxMultiplier { 2.0 };
 
-  double baseLTP{};
-  double baseLTD{};
+    double baseLTP {};
+    double baseLTD {};
 
-  double incrementLTP{};
-  double decrementLTD{};
+    double incrementLTP {};
+    double decrementLTD {};
 
-  double       alpha{};
-  double       beta{};
-  virtual void NormalizeWeights();
-  void         HardNormalize();
-  void         SoftMaxNormalize();
-  virtual void WeightDecay();
-  void         ThetaDecay();
-  void         UpdateCooperativity(signed long spineID, signed long neighborId);
-  // void pseudoCoop(signed long spineID, signed long neighborId);
+    double       alpha {};
+    double       beta {};
+    virtual void NormalizeWeights();
+    void         HardNormalize();
+    void         SoftMaxNormalize();
+    virtual void WeightDecay();
+    void         ThetaDecay();
+    void         UpdateCooperativity(signed long spineID, signed long neighborId);
+    // void pseudoCoop(signed long spineID, signed long neighborId);
 
-  virtual void UpdateLTP(signed long spineID) = 0;
-  virtual void UpdateLTD(signed long spineID) = 0;
+    virtual void UpdateLTP(signed long spineID) = 0;
+    virtual void UpdateLTD(signed long spineID) = 0;
 
-  virtual double gLTP(double deltaT) const = 0;
-  virtual double gLTD(double deltaT) const = 0;
+    virtual double gLTP(double deltaT) const = 0;
+    virtual double gLTD(double deltaT) const = 0;
 
-  virtual double aLTP(double theta) const;
-  virtual double aLTD(double theta) const;
+    virtual double aLTP(double theta) const;
+    virtual double aLTD(double theta) const;
 
-  double getDistanceEffects(const CoopSynapseSpine *const spineA, const CoopSynapseSpine *const spineB) const;
-  double getTimingEffects(const CoopSynapseSpine *const spineA, const CoopSynapseSpine *const spineB) const;
+    double getDistanceEffects(const CoopSynapseSpine *const spineA, const CoopSynapseSpine *const spineB) const;
+    double getTimingEffects(const CoopSynapseSpine *const spineA, const CoopSynapseSpine *const spineB) const;
 
-public:
-  explicit MonoDendriteSTDP(GlobalSimInfo *infoGlobal, const std::vector<FileEntry> &morphologyParameters);
-  ~MonoDendriteSTDP() override = default;
+  public:
+    explicit MonoDendriteSTDP(GlobalSimInfo *infoGlobal, const std::vector<FileEntry> &morphologyParameters);
+    ~MonoDendriteSTDP() override = default;
 
-  void Advect() override;
-  void Reset() override;
-  void RecordPostSpike() override;
-  void RecordExcitatoryPreSpike(BaseSpinePtr spinePtr) override;
+    void Advect() override;
+    void Reset() override;
+    void RecordPostSpike() override;
+    void RecordExcitatoryPreSpike(BaseSpinePtr spinePtr) override;
 
-  void SaveParameters(std::ofstream &wParameterStream, std::string neuronIdentificator) const override;
-  void LoadParameters(const std::vector<FileEntry> &parameters) override;
-  void CheckParameters(const std::vector<FileEntry> &parameters) override;
+    void SaveParameters(std::ofstream &wParameterStream, std::string neuronIdentificator) const override;
+    void LoadParameters(const std::vector<FileEntry> &parameters) override;
+    void CheckParameters(const std::vector<FileEntry> &parameters) override;
 
-  virtual BaseSpinePtr AllocateNewSynapse(BranchTargeting &bTargeting) override;
+    virtual BaseSpinePtr AllocateNewSynapse(BranchTargeting &bTargeting) override;
 
-  int    GetMaxGapDelay(int delayPerMicroMeter) override { return dendriticLength * delayPerMicroMeter; };
-  double GetSynapticDistanceToSoma(int synapseID) override { return spineDataCoop.at(synapseID)->GetDistToSoma(); };
-  // Revirtualization
+    // int    GetMaxGapDelay(int delayPerMicroMeter) override { return dendriticLength * delayPerMicroMeter; };
+    // double GetSynapticDistanceToSoma(int synapseID) override { return spineDataCoop.at(synapseID)->GetDistToSoma(); };
+    // Revirtualization
 };
 
-#endif // NEURALNETWORK_MONODENDRITE_H
+#endif  // NEURALNETWORK_MONODENDRITE_H
